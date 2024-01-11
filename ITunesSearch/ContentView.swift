a24ce4ecdcf0f527b159faa2bc45a4f4988f39c1
dongdong <93483694+dongdong867@users.noreply.github.com>
2024-01-11 22:23:24 +0800

    Initial Commit

---
+++ ITunesSearch/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ITunesSearch
+//
+//  Created by 東東 on 2024/1/11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ITunesSearch/ITunesSearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  ITunesSearchApp.swift
+//  ITunesSearch
+//
+//  Created by 東東 on 2024/1/11.
+//
+
+import SwiftUI
+
+@main
+struct ITunesSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
