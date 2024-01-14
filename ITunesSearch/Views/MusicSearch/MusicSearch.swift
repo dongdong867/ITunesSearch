72a3ae69ec698fd1f5bf64abb65fd36af9b926ee
dongdong <93483694+dongdong867@users.noreply.github.com>
2024-01-15 01:26:57 +0800

    feat(MusicSearch): implement search func with api call

--- ITunesSearch/Views/MusicSearch/MusicSearch.swift
+++ ITunesSearch/Views/MusicSearch/MusicSearch.swift
@@ -17,6 +17,38 @@ final class MusicSearch: ObservableObject {
     init() {
         querySubscription = $query
             .debounce(for: .seconds(0.3), scheduler: RunLoop.main)
-            .sink { print($0) }
+            .sink { [weak self] text in
+                if(!text.isEmpty) {
+                    self?.result = []
+                    self?.searchMusic(by: text)
+                }
+            }
     }
+    
+    func searchMusic(by query: String, page: Int = 1) -> Future<Void, Error> {
+        return Future<Void, Error> { [weak self] promise in
+            let endpoint = "https://itunes.apple.com/search?term=\(query)&media=music&offset=\((page-1)*10)&limit=\(page*10)"
+            
+            guard let url = URL(string: endpoint) else {
+                return promise(.failure(ApiError.invalidURL))
+            }
+            
+            URLSession.shared.dataTask(with: url) { [weak self] data, response, error in
+                if let data = data {
+                    do {
+                        let decoder = JSONDecoder()
+                        decoder.dateDecodingStrategy = .iso8601
+                        let rawData = try decoder.decode(MusicResponse.self, from: data)
+                        self?.result.append(contentsOf: rawData.musics)
+                    } catch {
+                        print("invalid data")
+                    }
+                } else if let error = error {
+                    print(error)
+                }
+            }.resume()
+                
+        }
+    }
+    
 }
