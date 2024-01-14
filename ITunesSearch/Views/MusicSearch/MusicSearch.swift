ab943bbb952f2770afb2e2d51b38e06761c86525
dongdong <93483694+dongdong867@users.noreply.github.com>
2024-01-15 03:57:19 +0800

    feat(MusicSearch): implement state usage and error handler

--- ITunesSearch/Views/MusicSearch/MusicSearch.swift
+++ ITunesSearch/Views/MusicSearch/MusicSearch.swift
@@ -9,18 +9,24 @@ import SwiftUI
 import Combine
 
 final class MusicSearch: ObservableObject {
+    
     @Published var result: [Music] = []
     @Published var query: String = ""
+    @Published var state: SearchState = .empty
     
     private var querySubscription: AnyCancellable?
     
     init() {
         querySubscription = $query
             .debounce(for: .seconds(0.3), scheduler: RunLoop.main)
-            .sink { [weak self] text in
+            .sink { text in
                 if(!text.isEmpty) {
-                    self?.result = []
-                    self?.searchMusic(by: text)
+                    self.result = []
+                    self.state = .loading
+                    self.searchMusic(by: text)
+                } else {
+                    self.result = []
+                    self.state = .empty
                 }
             }
     }
@@ -30,6 +36,7 @@ final class MusicSearch: ObservableObject {
             let endpoint = "https://itunes.apple.com/search?term=\(query)&media=music&offset=\((page-1)*10)&limit=\(page*10)"
             
             guard let url = URL(string: endpoint) else {
+                self?.state = .error(ApiError.invalidURL)
                 return promise(.failure(ApiError.invalidURL))
             }
             
@@ -39,12 +46,14 @@ final class MusicSearch: ObservableObject {
                         let decoder = JSONDecoder()
                         decoder.dateDecodingStrategy = .iso8601
                         let rawData = try decoder.decode(MusicResponse.self, from: data)
+                        
                         self?.result.append(contentsOf: rawData.musics)
+                        self?.state = .good
                     } catch {
-                        print("invalid data")
+                        self?.state = .error(ApiError.invalidData)
                     }
                 } else if let error = error {
-                    print(error)
+                    self?.state = .error(ApiError.unknown(error))
                 }
             }.resume()
                 
