f435858d0aa6c6dc345b28365a43d81743903e0c
dongdong <93483694+dongdong867@users.noreply.github.com>
2024-01-23 04:25:37 +0800

    feat(ArtistDetailView): split view to subviews and add style

--- ITunesSearch/Views/Details/ArtistDetail/ArtistDetailView.swift
+++ ITunesSearch/Views/Details/ArtistDetail/ArtistDetailView.swift
@@ -19,76 +19,105 @@ struct ArtistDetailView: View {
     var body: some View {
         ScrollView {
             Section {
-                VStack(alignment: .leading) {
-                    Text("\(artist.artistName)")
-                    Section {
-                        VStack(spacing: 14) {
-                            ForEach(tracks[0...min(3, tracks.count - 1)]) { track in
-                                MusicCardView(music: track)
-                            }
-                        }
-                    } header: {
-                        HStack {
-                            Text("All Songs")
-                            Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
-                                Image(systemName: "chevron.forward")
-                            }
-                        }
-                    }
-                    Section {
-                        HStack(spacing: 14) {
-                            ForEach(albums) { album in
-                                AlbumCardView(album: album)
-                                    .frame(maxWidth: 100)
-                            }
-                        }
-                    } header: {
-                        HStack {
-                            Text("Albums")
-                            Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
-                                Image(systemName: "chevron.forward")
-                            }
-                        }
-                    }
-                }
-                .frame(maxWidth: .infinity, alignment: .leading)
-                .padding()
+                artistContent
             } header: {
-                GeometryReader { geometry in
-                    Color.clear.preference(key: SizePreferenceKey.self, value: geometry.frame(in: .named("area")).minY)
-                }
-                .scaledToFit()
-                .onPreferenceChange(SizePreferenceKey.self) {
-                    scrollOffset = $0
-                }
+                avatarSizeHandler
             }
         }
         .coordinateSpace(name: "area")
         .overlay(alignment: .top) {
-            GeometryReader { geometry in
-                AsyncImage(url: avatar) { parse in
-                    switch parse {
-                        case .success(let image):
-                            image
-                                .resizable()
-                                .scaledToFit()
-                        default:
-                            Rectangle()
-                    }
-                }
-                .task {
-                    avatar = await artist.getArtistURL()
-                }
-                .scaledToFill()
-                .padding(.horizontal, min(0, -scrollOffset))
-                .frame(height: geometry.size.height + max(0, scrollOffset))
-                .offset(CGSize(width: 0, height: min(0, scrollOffset)))
-            }
-            .scaledToFit()
+            artistAvatar
         }
     }
     
-    struct SizePreferenceKey: PreferenceKey {
+    
+    var artistContent: some View {
+        VStack(alignment: .leading) {
+            Text("\(artist.artistName)")
+                .font(.title)
+                .fontWeight(.bold)
+            allSongs
+            allAlbums
+        }
+        .frame(maxWidth: .infinity, alignment: .leading)
+        .padding()
+    }
+    
+    var allSongs: some View {
+        Section {
+            VStack(spacing: 14) {
+                ForEach(tracks[0...min(3, tracks.count - 1)]) { track in
+                    MusicCardView(music: track)
+                }
+            }
+        } header: {
+            HStack {
+                Text("All Songs")
+                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                    Image(systemName: "chevron.forward")
+                }
+            }
+            .font(.headline)
+            .padding(.top, 4)
+        }
+    }
+    
+    var allAlbums: some View {
+        Section {
+            ScrollView(.horizontal) {
+                HStack(spacing: 14) {
+                    ForEach(albums) { album in
+                        AlbumCardView(album: album)
+                            .frame(width: 150)
+                    }
+                }
+            }
+        } header: {
+            HStack {
+                Text("Albums")
+                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                    Image(systemName: "chevron.forward")
+                }
+            }
+            .font(.headline)
+            .padding(.top, 10)
+        }
+    }
+    
+    var avatarSizeHandler: some View {
+        GeometryReader { geometry in
+            Color.clear.preference(key: SizePreferenceKey.self, value: geometry.frame(in: .named("area")).minY)
+        }
+        .scaledToFit()
+        .onPreferenceChange(SizePreferenceKey.self) {
+            scrollOffset = $0
+        }
+    }
+    
+    var artistAvatar: some View {
+        GeometryReader { geometry in
+            AsyncImage(url: avatar) { parse in
+                switch parse {
+                    case .success(let image):
+                        image
+                            .resizable()
+                            .scaledToFit()
+                    default:
+                        Rectangle()
+                }
+            }
+            .task {
+                avatar = await artist.getArtistURL()
+            }
+            .scaledToFill()
+            .padding(.horizontal, min(0, -scrollOffset))
+            .frame(height: geometry.size.height + max(0, scrollOffset))
+            .offset(CGSize(width: 0, height: min(0, scrollOffset)))
+        }
+        .scaledToFit()
+    }
+    
+    private struct SizePreferenceKey: PreferenceKey {
         typealias Value = CGFloat
         
         static var defaultValue: CGFloat = .zero
