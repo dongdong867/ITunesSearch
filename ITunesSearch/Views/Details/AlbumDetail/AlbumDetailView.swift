d1347c22ed25699f26ccd4f56070225ba4600493
dongdong <93483694+dongdong867@users.noreply.github.com>
2024-01-23 22:10:30 +0800

    feat(AlbumDetailView): create album detailview file

---
+++ ITunesSearch/Views/Details/AlbumDetail/AlbumDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  AlbumDetailView.swift
+//  ITunesSearch
+//
+//  Created by Dong on 2024/1/23.
+//
+
+import SwiftUI
+
+struct AlbumDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AlbumDetailView()
+}
