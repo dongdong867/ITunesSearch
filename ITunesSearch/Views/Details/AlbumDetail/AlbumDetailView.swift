4b398bc0d101dbc482541c6e77325fd7bc522fa0
dongdong <93483694+dongdong867@users.noreply.github.com>
2024-01-23 23:05:39 +0800

    feat(AlbumDetailView): create basic view structure

--- ITunesSearch/Views/Details/AlbumDetail/AlbumDetailView.swift
+++ ITunesSearch/Views/Details/AlbumDetail/AlbumDetailView.swift
@@ -8,11 +8,43 @@
 import SwiftUI
 
 struct AlbumDetailView: View {
+    let album: Album
+    let tracks: [Music]
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            AsyncImage(url: album.artworkUrl100) { parse in
+                switch parse {
+                    case .success(let image):
+                        image
+                            .resizable()
+                            .scaledToFit()
+                    default:
+                        Rectangle()
+                }
+            }
+            .aspectRatio(1, contentMode: .fit)
+            .frame(maxWidth: 240)
+            
+            Text(album.collectionName)
+            Text(album.artistName)
+            Text("\(album.primaryGenreName)．\(String(getReleaseDate(releaseDate:album.releaseDate).prefix(4)))")
+            
+            List(0..<tracks.count, id: \.self) { index in
+                HStack {
+                    Text(String(format: "%2d", index+1))
+                    Text("\(tracks[index].trackName)")
+                }
+            }
+            .listStyle(.plain)
+            
+            Text(getReleaseDate(releaseDate:album.releaseDate))
+            Text("\(album.trackCount) songs")
+            Text(album.copyright)
+        }
     }
 }
 
 #Preview {
-    AlbumDetailView()
+    AlbumDetailView(album: Album.sampleData, tracks: Music.sampleList)
 }
